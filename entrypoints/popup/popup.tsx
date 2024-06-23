a337371bfe6e88812990c61e6b2e3dc61faee3d4
HotariTobu <hotari24tools@gmail.com>
2024-06-23 10:08:09 +0900

    add: api key box

--- entrypoints/popup/popup.tsx
+++ entrypoints/popup/popup.tsx
@@ -1,5 +1,18 @@
+import { useState, useEffect } from "react";
+import { apiKeyItem } from "./storageItems";
+
 export const Popup = () => {
+  const [apiKey, setApiKey] = useState('');
+
+  useEffect(() => {
+    apiKeyItem.getValue().then(setApiKey)
+  }, [])
+
+  const applyApiKeyChange = async () => {
+    await apiKeyItem.setValue(apiKey)
+  }
+
   return (
-    <></>
-  )
+    <input value={apiKey} onChange={e => setApiKey(e.target.value)} onBlur={applyApiKeyChange} placeholder='GOOGLE_GENERATIVE_AI_API_KEY' />
+  );
 }
