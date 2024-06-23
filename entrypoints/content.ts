6acb51a74b6d376c65795b1aaa57547e22da77f1
HotariTobu <hotari24tools@gmail.com>
2024-06-23 10:43:40 +0900

    add: answer panel

--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -5,9 +5,24 @@ import { apiKeyItem } from "./popup/storageItems";
 export default defineContentScript({
   matches: ['<all_urls>'],
   main() {
+    addAnswerPanel()
     addMessageReceiver()
   },
 });
+
+const addAnswerPanel = () => {
+  const answerPanel = document.createElement('div')
+  document.body.appendChild(answerPanel)
+  answerPanel.style.position = 'sticky'
+  answerPanel.style.left = "0"
+  answerPanel.style.right = "0"
+  answerPanel.style.bottom = "0"
+  answerPanel.style.height = "200px"
+  answerPanel.style.overflowY = "auto"
+  answerPanel.style.whiteSpace = "break-spaces"
+  answerPanel.style.background = "white"
+  answerPanel.style.opacity = "0.8"
+}
 
 const addMessageReceiver = () => {
   browser.runtime.onMessage.addListener((_, sender) => {
