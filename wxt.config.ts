3876d608f5b3f96554840b4d75209b9d82682a24
HotariTobu <hotari24tools@gmail.com>
2024-06-23 10:05:41 +0900

    add: storage permission

--- wxt.config.ts
+++ wxt.config.ts
@@ -2,5 +2,8 @@ import { defineConfig } from 'wxt';
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
+  manifest: {
+    permissions: ['storage'],
+  },
   modules: ['@wxt-dev/module-react'],
 });
