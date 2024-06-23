314b92a04982f2798f6672c53262ae54cc304e1f
HotariTobu <hotari24tools@gmail.com>
2024-06-23 10:11:15 +0900

    add: context menu handler

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,5 +1,6 @@
 export default defineBackground(() => {
   browser.runtime.onInstalled.addListener(addAskContextMenu)
+  addContextMenuHandler()
 });
 
 const addAskContextMenu = () => {
@@ -7,5 +8,14 @@ const addAskContextMenu = () => {
     id: 'ask-to-ai',
     contexts: ['selection'],
     title: 'Ask to AI'
+  })
+}
+
+const addContextMenuHandler = () => {
+  browser.contextMenus.onClicked.addListener(async (info, tab) => {
+    if (typeof tab?.id === 'undefined') {
+      return
+    }
+    await browser.tabs.sendMessage(tab.id, info)
   })
 }
