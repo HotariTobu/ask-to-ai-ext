d1d7e9396ef5f3808d30303704231a9e01be391e
HotariTobu <hotari24tools@gmail.com>
2024-07-10 01:11:27 +0900

    add: overlay icon

--- entrypoints/content/overlay.tsx
+++ entrypoints/content/overlay.tsx
@@ -1,14 +1,35 @@
+import icon from '@/assets/icon.svg'
+
 export const Overlay = (props: {
   rects: DOMRect[]
 }) => {
-  return <>
-    {props.rects.map(rect => (
-      <div className="bg-red-500/10 absolute pointer-events-none" style={{
-        left: rect.left,
-        top: rect.top,
-        width: rect.width,
-        height: rect.height,
-      }} key={JSON.stringify(rect)} />
-    ))}
-  </>
+  const lastRect = props.rects.findLast(rect => rect.width > 0)
+  if (typeof lastRect === 'undefined') {
+    return
+  }
+
+  return (
+    <>
+      {props.rects.map(rect => (
+        <div className="bg-[#ff0]/50 absolute pointer-events-none" style={{
+          left: rect.left,
+          top: rect.top,
+          width: rect.width,
+          height: rect.height,
+        }} key={JSON.stringify(rect)} />
+      ))}
+      <div className="absolute z-[9999]" style={{
+        left: lastRect.right,
+        top: lastRect.top,
+      }} >
+        <div className='drop-shadow-md'>
+          <div className='p-[4px] bg-white' style={{
+            clipPath: 'circle(50%)'
+          }}>
+            <img width={16} height={16} src={icon} />
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
