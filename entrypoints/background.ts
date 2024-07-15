2d7bee486d4f3e05a18acd672d92967c376202e4
HotariTobu <hotari24tools@gmail.com>
2024-07-16 01:46:30 +0900

    add: open options page message

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,7 +1,26 @@
+import { Menus, Runtime, Tabs } from "wxt/browser";
+
 export default defineBackground(() => {
+  browser.runtime.onMessage.addListener(handleMessage)
   browser.runtime.onInstalled.addListener(addAskContextMenu)
-  addContextMenuHandler()
+  browser.contextMenus.onClicked.addListener(handleContextMenuClick)
 });
+
+export type Message = {
+  action: 'open-options-page'
+}
+
+const handleMessage = async (message: Message, sender: Runtime.MessageSender) => {
+  if (browser.runtime.id !== sender.id) {
+    return
+  }
+
+  switch (message.action) {
+    case 'open-options-page':
+      await browser.runtime.openOptionsPage()
+      break
+  }
+}
 
 const addAskContextMenu = () => {
   browser.contextMenus.create({
@@ -11,11 +30,9 @@ const addAskContextMenu = () => {
   })
 }
 
-const addContextMenuHandler = () => {
-  browser.contextMenus.onClicked.addListener(async (info, tab) => {
-    if (typeof tab?.id === 'undefined') {
-      return
-    }
-    await browser.tabs.sendMessage(tab.id, info)
-  })
+const handleContextMenuClick = async (info: Menus.OnClickData, tab: Tabs.Tab | undefined) => {
+  if (typeof tab?.id === 'undefined') {
+    return
+  }
+  await browser.tabs.sendMessage(tab.id, info)
 }
